26e9386f8458f3fe69e1e19b617e24dbc16a1aba
khajak2004 <khajak.ghazaryan.y@tumo.org>
2019-12-23 19:24:23 +0400

    Add photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 0) do
-=======
-ActiveRecord::Schema.define(version: 2019_12_20_150333) do
+ActiveRecord::Schema.define(version: 2019_12_23_145337) do
+
+  create_table "photos", force: :cascade do |t|
+    t.text "description"
+    t.string "title"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,6 +32,6 @@ ActiveRecord::Schema.define(version: 2019_12_20_150333) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
->>>>>>> 9bbe17dbc732af9d56ca2a03a18e220b137061e6
 
+  add_foreign_key "photos", "users"
 end
