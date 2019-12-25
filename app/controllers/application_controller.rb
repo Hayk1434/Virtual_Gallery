8826f3e8fe377c79f2a3945736472e7951a92a7b
Hayk1434 <sahakyan1434@gmail.com>
2019-12-25 17:18:56 +0400

    okay

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    # before_action :configure_permitted_parameters, if: :devise_controller?
+
+    # protected
+
+    #     def configure_permitted_parameters
+    #         devise_parameter_sanitizer.for(:sign_up).push(:name, :surname,:username, :email, :avatar)
+    #         devise_parameter_sanitizer.for(:account_update).push(:name, :surname, :email, :avatar)
+    #     end
 end
