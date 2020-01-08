8ff775a8d18e0491b7756e7274e6df9d9893ce2e
Hayk1434 <sahakyan1434@gmail.com>
2020-01-08 19:13:36 +0400

    hamarya verj

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @photos = User.photos
+    @photos = @user.photos
   end
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :file
+  validates :file, presence: true
+  validates :title, presence: true
 end
