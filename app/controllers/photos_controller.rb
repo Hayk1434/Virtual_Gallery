2dabac82a5808e18f5104ce1bdb467e5f3fe1615
Hayk1434 <sahakyan1434@gmail.com>
2020-01-09 19:30:24 +0400

    ready

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -28,7 +28,7 @@ class PhotosController < ApplicationController
 
     respond_to do |format|
       if @photo.save
-        format.html { redirect_to @photo, notice: 'Photo was successfully created.' }
+        format.html { redirect_to current_user, notice: 'Photo was successfully created.' }
         format.json { render :show, status: :created, location: @photo }
       else
         format.html { render :new }
